12bd84b929048df3404b34baf720c42e5ae2568c
Aditi <aditidevani528@gmail.com>
2024-05-20 15:48:14 +0530

    add -fo;es

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const mongoose = require("mongoose");
 var cors = require("cors");
 const multer = require("multer");
 
-
 const app = express();
 app.use(cors());
 const PORT = 8000;
+
+app.use(express.static(path.resolve('upload')))
+
 
 // Connect to MongoDB
 mongoose
@@ -35,25 +37,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
-
 app.get("/", (req, res) => {
   res.send("Hello, Swiggy!");
 });
 
 app.post("/add-product", upload.single("productImg"), async (req, res) => {
-  console.log(req.file);
-  console.log(req.body)
-  const createdProduct = await productModel.create({...req.body, productImg : req.file.path });
+  console.log(req.body);
+  const productImg = "http://localhost:8000/image/" + req.file.filename;
+  const createdProduct = await productModel.create({ ...req.body, productImg});
   return res.send(createdProduct);
-}); 
+});
 
-
-app.get("/all-product" , async (req , res) =>{  
+app.get("/all-product", async (req, res) => {
   const product = await productModel.find({});
-  console.log(product)
-  return res.json(product)
-})
+  console.log(product);
+  return res.json(product);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
